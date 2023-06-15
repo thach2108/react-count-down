1e221804562991f183caf078b9087bb4c95f237b
ThachDD <thachdd.fe@gmail.com>
2023-06-15 09:33:06 +0700

    update

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,21 +5,26 @@ import ExampleLine from './pages/Line'
 
 const App = () => {
   return (
-    <div className='mx-auto max-w-screen-xl mt-8 mb-16 px-4'>
+    <div className='mx-auto max-w-screen-xl px-4'>
       <BrowserRouter>
         <Suspense fallback={<div>Loading...</div>}>
           <div className='grid grid-cols-10 gap-4'>
-            <div className='col-span-2'>
-              <ul className='pl-5'>
-                <li className='text-lg'>
-                  <Link to='/react-count-down'>Circle animation</Link>
-                </li>
-                <li className='text-lg'>
-                  <Link to='/react-count-down/line'>Line animation</Link>
-                </li>
-              </ul>
+            <div className='col-span-2 h-screen'>
+              <div className='box-border fixed pt-3 pb-8 flex flex-col justify-between h-full top-0'>
+                <ul className='pl-5'>
+                  <li className='text-lg'>
+                    <Link to='/react-count-down'>Circle animation</Link>
+                  </li>
+                  <li className='text-lg'>
+                    <Link to='/react-count-down/line'>Line animation</Link>
+                  </li>
+                </ul>
+                <a href='https://github.com/thach2108/react-count-down-animation'>
+                  Github reponsitory
+                </a>
+              </div>
             </div>
-            <div className='col-span-8'>
+            <div className='col-span-8 mt-8 mb-10'>
               <Routes>
                 <Route path='/' element={<ExampleCircle />} />
                 <Route path='/react-count-down' element={<ExampleCircle />} />
